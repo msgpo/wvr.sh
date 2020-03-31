7d82ec2bb342ac8f096a3968b02dbdb29d4a3498
Mitch Weaver <mitch@wvr.sh>
2020-03-31 10:14:33 -0500

    build: cleanup, add support for wiki

--- build.sh
+++ build.sh
@@ -8,8 +8,9 @@ OUT=./www/html
 # -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 md2html() {
-    discount -f autolink,smarty,fencedcode,toc,tabstop,footnote,\
-image,links,superscript,strikethrough "$1"
+    discount -f autolink,smarty,fencedcode,toc,tabstop,\
+footnote,image,links,superscript,strikethrough,\
+definitionlist,dldiscount,dlextra "$*"
 }
 
 init() {
@@ -18,20 +19,46 @@ init() {
     cp -r $SRC/res $OUT/
 }
 
+add() {
+    tag=$1
+    shift
+    printf "<$tag>\n%s\n</$tag>\n" "$*"
+}
+
 build() {
     find $SRC -type f -name '*.md' | \
     while read -r file ; do
+        # trim the src dir from filename
         file=${file#$SRC/}
+        # make diretory path if needed
         mkdir -p $OUT/${file%/*}
+        # remove .md file extension
         file=${file%.md}
+
+        # index files need to keep their .html extension to function.
+        # other files can be linked without extension for ease of use
+        # (and to keep vimwiki able to link within itself)
+        [ ${file##*/} = index ] && ext=.html
+
         {
+            # add the metadata information
             cat $SRC/head.html
+
+            # use filename as page title
             printf '<title>%s</title>\n</head>' ${file%/*}
-            printf '<header><p>%s</p></header>\n' "$(md2html $SRC/header.md)"
-            printf '<body>%s</body>\n' "$(md2html $SRC/$file.md)"
-            printf '<footer><p>%s</p></footer>\n</html>\n' "$(md2html $SRC/footer.md)"
-        } >>$OUT/$file.html &
+
+            add header "$(md2html $SRC/header.md)"
+            add body   "$(md2html $SRC/$file.md)"
+            add footer "$(md2html $SRC/footer.md)"
+
+            # close document
+            printf '%s\n' '</html>'
+        } >>$OUT/$file$ext &
+
+        # unset ext as not to carry over to next file
+        unset ext
     done
+    # wait for conversion jobs to finish
     wait
 }
 
